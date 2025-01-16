cb0eed09adf0239c918b3f1639d78685795e63ff
Efrai <efrainklabef@gmail.com>
2025-01-16 13:11:14 -0300

    first Commit

---
+++ dataschedule/src/main/java/com/example/dataschedule/AgendamentosDeDataShowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.dataschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosDeDataShowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosDeDataShowsApplication.class, args);
+	}
+
+}
---
+++ dataschedule/src/test/java/com/example/dataschedule/AgendamentosDeDataShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dataschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosDeDataShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
