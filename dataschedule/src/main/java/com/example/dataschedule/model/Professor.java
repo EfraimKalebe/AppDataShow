faa9c442c584b1c480bbaf497e97f4a181f43280
Efrai <efrainklabef@gmail.com>
2025-01-17 13:04:30 -0300

    Criação de Repository

--- dataschedule/src/main/java/com/example/dataschedule/model/Professor.java
+++ dataschedule/src/main/java/com/example/dataschedule/model/Professor.java
@@ -11,4 +11,13 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String senha;
+
 }
---
+++ dataschedule/src/main/java/com/example/dataschedule/repository/AgendamentoRepository.java
@@ -0,0 +1,15 @@
+package com.example.dataschedule.repository;
+
+import com.example.dataschedule.model.Agendamento;
+
+import org.springframework.cglib.core.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+    List<Agendamento> findByDataHoraInicioBetweenOrDataHoraFimBetween(
+            LocalDateTime start, LocalDateTime end, LocalDateTime start2, LocalDateTime end2);   
+}
